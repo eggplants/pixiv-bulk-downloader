2c65ecc10b7affb138e0adbfe2c4b948fb2b7899
eggplants <w10776e8w@yahoo.co.jp>
2020-11-04 17:21:26 +0900

    0.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ python -m twine upload --repository pypi dist/*
 
 setup(
     name='pixiv-bulk-downloader',
-    version='0.0.2',
+    version='0.0.3',
     description='Pixiv Bulk Downloader',
     description_content_type='',
     long_description=open('README.md').read(),
