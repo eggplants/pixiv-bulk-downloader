144cb67d497aea1512e28433cc340aefc963c081
eggplants <w10776e8w@yahoo.co.jp>
2021-09-04 00:16:14 +0900

    v1.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def parse_requires():
 
 setup(
     name='pixiv-bulk-downloader',
-    version='1.5',
+    version='1.6',
     description='Pixiv Bulk Downloader',
     description_content_type='',
     long_description=open('README.md').read(),
